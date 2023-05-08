5be41246a653e29a0e4d311f0a2613400bb19671
MenesK2M <117518027+MenesK2M@users.noreply.github.com>
2023-05-08 14:13:28 -0400

    Update jenkins_master.sh

--- jenkins Master and slaves/jenkins_master.sh
+++ jenkins Master and slaves/jenkins_master.sh
@@ -2,5 +2,3 @@
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum install -y jenkins
-
-sudo systemctl enable --now jenkins
